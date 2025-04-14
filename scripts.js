80260ddef1d4d46313e960cdff6c4673ce063d70
Juan <valdezjuan12341@gmail.com>
2025-04-14 16:32:23 -0700

    Implmented visual changes for css and html, adding visual updates and adding two new testing buttons. In js file added data in form of a array of objects and added functions sortDates,SortSeries, and SortGundamName that allows sorting by object's dates, series, and by name of gundam model kit. Updated showCards to print from this new object array by iterating through it.

--- scripts.js
+++ scripts.js
@@ -23,6 +23,91 @@
  *
  */
 
+
+let gundam_kits = [
+      {
+        "name": "RX-78-2 Gundam (HG 1/144)",
+        "grade": "High Grade",
+        "release_year": 1980,
+        "price_yen": 300,
+        "series": "Mobile Suit Gundam",
+        "price_usd": 2.01,
+        "image_url": "https://m.media-amazon.com/images/I/71jb0jBaPGL.jpg"
+      },
+      {
+        "name": "ZGMF-X10A Freedom Gundam (HG 1/144)",
+        "grade": "High Grade",
+        "release_year": 2004,
+        "price_yen": 1200,
+        "series": "Mobile Suit Gundam SEED",
+        "price_usd": 8.04,
+        "image_url": "https://i.ebayimg.com/images/g/JsgAAOSwYRpjndne/s-l1200.jpg"
+      },
+      {
+        "name": "ASW-G-08 Gundam Barbatos (HG 1/144)",
+        "grade": "High Grade",
+        "release_year": 2015,
+        "price_yen": 1000,
+        "series": "Mobile Suit Gundam: Iron-Blooded Orphans",
+        "price_usd": 6.7,
+        "image_url": "https://m.media-amazon.com/images/I/71XivClnE2L._AC_UF894,1000_QL80_.jpg"
+      },
+      {
+        "name": "RX-78-2 Gundam Ver. 3.0 (MG 1/100)",
+        "grade": "Master Grade",
+        "release_year": 2013,
+        "price_yen": 4500,
+        "series": "Mobile Suit Gundam",
+        "price_usd": 30.15,
+        "image_url": "https://i.ebayimg.com/images/g/ORsAAOSwU-hjBYwO/s-l1200.jpg"
+      },
+      {
+        "name": "ZGMF-X20A Strike Freedom Gundam (MG 1/100)",
+        "grade": "Master Grade",
+        "release_year": 2006,
+        "price_yen": 5000,
+        "series": "Mobile Suit Gundam SEED Destiny",
+        "price_usd": 33.5,
+        "image_url": "https://cdn.shopify.com/s/files/1/2786/5582/products/mg-zgmf-x20a-strike-freedom-gundam-pa.jpg?v=1657133255"
+      },
+      {
+        "name": "XXXG-00W0 Wing Gundam Zero EW Ver. (MG 1/100)",
+        "grade": "Master Grade",
+        "release_year": 2010,
+        "price_yen": 3800,
+        "series": "Mobile Suit Gundam Wing: Endless Waltz",
+        "price_usd": 25.46,
+        "image_url": "https://cdn.shopify.com/s/files/1/2786/5582/products/mg-xxxg-00w0-wing-gundam-zero-custom-pa.jpg?v=1631227344"
+      },
+      {
+        "name": "RX-78-2 Gundam (PG 1/60)",
+        "grade": "Perfect Grade",
+        "release_year": 1998,
+        "price_yen": 12000,
+        "series": "Mobile Suit Gundam",
+        "price_usd": 80.4,
+        "image_url": "https://i.ebayimg.com/images/g/AqQAAOSwfQljm7Sl/s-l1200.png"
+      },
+      {
+        "name": "ZGMF-X10A Freedom Gundam (PG 1/60)",
+        "grade": "Perfect Grade",
+        "release_year": 2004,
+        "price_yen": 15000,
+        "series": "Mobile Suit Gundam SEED",
+        "price_usd": 100.5,
+        "image_url": "https://static.wikia.nocookie.net/gunplabuilders/images/1/17/RG_Freedom_Gundam_boxart.jpg/revision/latest?cb=20170609043716"
+      },
+      {
+        "name": "MBF-P02 Gundam Astray Red Frame (PG 1/60)",
+        "grade": "Perfect Grade",
+        "release_year": 2009,
+        "price_yen": 18000,
+        "series": "Mobile Suit Gundam SEED Astray",
+        "price_usd": 120.6,
+        "image_url": "https://lascalemodel.com/cdn/shop/products/PG-Gundam-Astray-Red-Frame-box-2.webp?v=1676517656"
+      }
+];
+
 const FRESH_PRINCE_URL =
   "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
 const CURB_POSTER_URL =
@@ -35,6 +120,7 @@ let titles = [
   "Fresh Prince of Bel Air",
   "Curb Your Enthusiasm",
   "East Los High",
+  "Hello",
 ];
 // Your final submission should have much more data than this, and
 // you should use more than just an array of strings to store it all.
@@ -45,26 +131,52 @@ function showCards() {
   cardContainer.innerHTML = "";
   const templateCard = document.querySelector(".card");
 
-  for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
+  for (let i = 0; i < gundam_kits.length; i++) {
+    let title = gundam_kits[i].name;
 
     // This part of the code doesn't scale very well! After you add your
     // own data, you'll need to do something totally different here.
     let imageURL = "";
-    if (i == 0) {
-      imageURL = FRESH_PRINCE_URL;
-    } else if (i == 1) {
-      imageURL = CURB_POSTER_URL;
-    } else if (i == 2) {
-      imageURL = EAST_LOS_HIGH_POSTER_URL;
-    }
+    
+    
 
     const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL); // Edit title and image
+    editCardContent(nextCard, title, gundam_kits[i].image_url); // Edit title and image
     cardContainer.appendChild(nextCard); // Add new card to the container
   }
 }
-
+function sortSeries(){
+  gundam_kits.sort((a,b) => {
+    if (a.series < b.series) return -1;
+    if (a.series > b.series) return 1;
+    return 0;
+  });
+  console.log(gundam_kits)
+  showCards();
+}
+function sortDates(){
+      gundam_kits.sort((a,b) => {
+        if (a.release_year < b.release_year) return -1;
+        if (a.release_year > b.release_year) return 1;
+        return 0;
+      })
+      console.log(gundam_kits)
+      showCards();
+};
+///Implemented 
+/// Function for filtering gundams by name, this will then be attached to a dropdown or button
+///Param: passing a gundam name 
+///manipulates the array then calls showCards function to display the manipulated array.
+function filterGundamName(gundam_name){
+  const lowerGundamName = gundam_name.toLowerCase();
+  for (let i = gundam_kits.length - 1; i>= 0; i--){
+    if (!gundam_kits[i].name.toLowerCase().includes(lowerGundamName)){
+        gundam_kits.splice(i,1);
+       
+    }
+    showCards();
+  }
+}
 function editCardContent(card, newTitle, newImageURL) {
   card.style.display = "block";
 
@@ -92,6 +204,6 @@ function quoteAlert() {
 }
 
 function removeLastCard() {
-  titles.pop(); // Remove last item in titles array
+  gundam_kits.pop(); // Remove last item in titles array
   showCards(); // Call showCards again to refresh
 }
